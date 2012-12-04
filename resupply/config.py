9bc62d5b9fcbfb449cba49cd65ff1a067ac36ee2
Imran Khawaja <imrank1@gmail.com>
2012-12-04 17:28:52 -0500

    modified config.py

--- resupply/config.py
+++ resupply/config.py
@@ -8,10 +8,10 @@ dev = {
 production = {
 	'stripe_secret_key': 'sk_07vkIYtFhTJY68s2pipRKmlvDtiqk',
 	'stripe_publishable_key': 'pk_07vkx4yqszys5bnTNnHPSAAimkCie',
-	'mongodb_db':'resupply_production',
-	'mongodb_username':'resupply_dev',
-	'mongodb_password':'imran',
-	'mongodb_host':'alex.mongohq.com',
-	'mongodb_port':10046,
-	'secret_key':'p0c0n0$CyrusImranOnlySirah'
+	'mongodb_db': 'resupply_production',
+	'mongodb_username': 'resupply_dev',
+	'mongodb_password': 'imran',
+	'mongodb_host': 'alex.mongohq.com',
+	'mongodb_port': 10046,
+	'secret_key': 'p0c0n0$CyrusImranOnlySirah'
 }
