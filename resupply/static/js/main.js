947b7ba4a32a80e7c553d7a1124bfd7aa17f23de
imrank1 <imrank1@gmail.com>
2013-02-24 14:48:36 -0500

    refactoring

--- resupply/static/js/main.js
+++ resupply/static/js/main.js
@@ -1,4 +1,4 @@
-require(['ext/jquery', 'ext/underscore', 'ext/backbone', 'ext/checkout', 'template', 'todos','signupCustom'], function() {
+require(['ext/jquery', 'ext/underscore', 'ext/backbone', 'ext/checkout', 'template','signupCustom'], function() {
     $(document).ready(function() {
         window.App = new Signup({ el: $('#billing_container') });
         window.App.render();
