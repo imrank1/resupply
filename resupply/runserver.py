ee71fc493a7e5099ff2f66116b35661202e97806
imrank1 <imrank1@gmail.com>
2013-06-02 01:22:43 -0400

    try flask sslify

--- resupply/runserver.py
+++ resupply/runserver.py
@@ -1,5 +1,15 @@
 import os
 from resupply import app
+from resupply import config
+env = os.environ.get('FLASK_ENV', 'development')
+
 port = int(os.environ.get('PORT', 5000))
 app.run(host='0.0.0.0', port=port)
-app.run(debug=True)
+
+
+
+if env =='development':
+	app.logger.info("Running in debug mode")
+	app.run(debug=True)
+else:
+	app.run()
--- runserver.py
+++ runserver.py
@@ -1,4 +1,17 @@
 import os
 from resupply import app
+from resupply import config
 port = int(os.environ.get('PORT', 5000))
-app.run(debug=True,host='0.0.0.0', port=port)
+#app.run(debug=True,host='0.0.0.0', port=port)
+
+env = os.environ.get('FLASK_ENV', 'development')
+
+#app.run(host='0.0.0.0', port=port)
+
+
+
+if env =='development':
+	print 'running in debug mode'
+	app.run(debug=True,host='0.0.0.0', port=port)
+else:
+	app.run()
