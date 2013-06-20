2aaaae6277a22c3e1e402c1fd043f22987d4e183
Dominick Pham <dominick.pham@gmail.com>
2013-06-19 23:08:35 -0400

    some refactoring

--- resupply/static/js/home.js
+++ resupply/static/js/home.js
@@ -1,24 +1,22 @@
 require.config({
 	baseUrl: 'static/js',
-  shim: {
-    underscore: {
-      exports: '_'
-    },
-    backbone: {
-      deps: ['underscore', 'jquery'],
-      exports: 'Backbone'
-    }
-  },
-  paths: {
-    jquery: 'ext/jquery',
-    underscore: 'ext/underscore',
-    backbone: 'ext/backbone',
-    templates: '../templates'
-  }
+	shim: {
+		underscore: {
+			exports: '_'
+		},
+		backbone: {
+			deps: ['underscore', 'jquery'],
+			exports: 'Backbone'
+		}
+	},
+	paths: {
+		jquery: 'ext/jquery',
+		underscore: 'ext/underscore',
+		backbone: 'ext/backbone',
+		templates: '../templates'
+	}
 
 });
-require(['homePage'], function() {
-    $(document).ready(function() {
-        window.HomePage = new HomePage({ el: $('#infoContainer') });
-    });
+require(['homePage'], function(HomePage) {
+	window.HomePage = new HomePage({ el: '#infoContainer' });
 });
--- resupply/static/js/homePage.js
+++ resupply/static/js/homePage.js
@@ -1,155 +1,147 @@
 define(['jquery','underscore','backbone'], function($,_,Backbone) {
-  var TEMPLATE_URL = '/static';
-    window.HomePage = Backbone.View.extend({
-        events: {
-            "click #getStarted": "processGetStarted",
-            "click #ontoPricing": "processGetStarted",
-            "click #ontoPricingUpgrade": "processGetStartedUpgrade",
-            "click #subscribe": "addToSubscribe"
-        },
+	var TEMPLATE_URL = '/static';
+	var HomePage = Backbone.View.extend({
+		events: {
+			"click #getStarted": "processGetStarted",
+			"click #ontoPricing": "processGetStarted",
+			"click #ontoPricingUpgrade": "processGetStartedUpgrade",
+			"click #subscribe": "addToSubscribe"
+		},
 
-        initialize: function(options) {
+		initialize: function(options) {
 
-        },
+		},
 
-        processGetStarted:function(e){
-            self = this;
-            e.preventDefault();
-            var missingFields = false;
-            var error=false;
-            $(".requiredStuff").each(function(){
-                if($(this).val()==""){
-                    $(this).addClass("errorState");
-                    missingFields=true;
-                }
-            });
+		processGetStarted:function(e){
+			var self = this;
+			e.preventDefault();
+			var missingFields = false;
+			var error=false;
+			$(".requiredStuff").each(function(){
+				var $input = $(this);
+				if(!$input.val()){
+					$input.addClass("errorState");
+					missingFields = true;
+				}
+			});
 
-            if(missingFields==true){
-                $("#getStartedErrorMessage").show();
-            }else {
+			if(missingFields) {
+				$("#getStartedErrorMessage").show();
+			} else {
+				var zipCode = $("#zipCode").val();
+				var numFamily = $("#numFamily").val();
+				var gender = $('input:radio[name=gender]:checked').val();
 
-            var zipCode = $("#zipCode").val();
-            var numFamily = $("#numFamily").val();
-            var gender = $('input:radio[name=gender]:checked').val();
+				$.ajax({
+					url: '/getStarted',
+					type: 'POST',
+					data:{zipCode:zipCode,numFamily:numFamily,gender:gender},
+					cache: false,
+					statusCode : {
+						200: function(){
+							window.location = '/pricing';
+							return this;
+						},
+						500: function(){
+							$("#cantShipThere").show("slow");
+							$("#subscribeBox").show("slow");
+							return false;
+						}
+					}
+				});
+				return false;
+			}
 
-            $.ajax({
-            url: '/getStarted',
-            type: 'POST',
-            data:{zipCode:zipCode,numFamily:numFamily,gender:gender},
-            cache: false,
-            statusCode : { 
-                200: function(){
-                window.location = '/pricing';    
-                return this;
-            },
-                500: function(){
-                    $("#cantShipThere").show("slow");
-                    $("#subscribeBox").show("slow");
-                    return false;
-            }
-          }
-        });
+			return this;
+		},
+		processGetStartedUpgrade:function(e){
+			var self = this;
+			e.preventDefault();
+			var missingFields = false;
+			var error=false;
+			$(".requiredStuff").each(function(){
+				var $input = $(this);
+				if(!$input.val()){
+					$input.addClass("errorState");
+					missingFields=true;
+				}
+			});
 
+			if(missingFields){
+				$("#getStartedErrorMessage").show();
+			}else {
 
+				var numFamily = $("#numFamily").val();
 
-            return false;
-        }
+				$.ajax({
+					url: '/getStartedUpgrade',
+					type: 'POST',
+					data:{numFamily:numFamily},
+					cache: false,
+					statusCode : {
+						200: function(){
+							window.location = '/pricing';
+							return this;
+						},
+						500: function(){
+							$("#subscribeBox").show("slow");
+							return false;
+						}
 
-        return this;
+					}
+				});
 
-        },
-         processGetStartedUpgrade:function(e){
-            self = this;
-            e.preventDefault();
-            var missingFields = false;
-            var error=false;
-            $(".requiredStuff").each(function(){
-                if($(this).val()==""){
-                    $(this).addClass("errorState");
-                    missingFields=true;
-                }
-            });
+				return false;
+			}
 
-            if(missingFields==true){
-                $("#getStartedErrorMessage").show();
-            }else {
+			return this;
 
-            var numFamily = $("#numFamily").val();
+		},
+		addToSubscribe:function(e){
+			var self = this;
+			e.preventDefault();
+			var missingFields = false;
+			var error=false;
+			$(".requiredSforSubscribe").each(function(){
+				var $input = $(this);
+				if(!$input.val()){
+					$input.addClass("errorState");
+					missingFields=true;
+				}
+			});
 
-            $.ajax({
-            url: '/getStartedUpgrade',
-            type: 'POST',
-            data:{numFamily:numFamily},
-            cache: false,
-            statusCode : { 
-                200: function(){
-                window.location = '/pricing';    
-                return this;
-            },
-            500: function(){
-                    $("#subscribeBox").show("slow");
-                    return false;
-            }
+			if(missingFields){
+				$("#subscribeErrorMessage").show();
+			}else {
 
-          }
-        });
-
-
-
-            return false;
-        }
-
-        return this;
-
-        },
-        addToSubscribe:function(e){
-            self = this;
-            e.preventDefault();
-            var missingFields = false;
-            var error=false;
-            $(".requiredSforSubscribe").each(function(){
-                if($(this).val()==""){
-                    $(this).addClass("errorState");
-                    missingFields=true;
-                }
-            });
-
-            if(missingFields==true){
-                $("#subscribeErrorMessage").show();
-            }else {
-
-            var email = $("#emailAddressSubscribe").val();
-           
-
-            $.ajax({
-            url: '/addToSubscribe',
-            type: 'POST',
-            data:{email:email,},
-            cache: false,
-            statusCode : { 
-                200: function(){
-                $("#subscribeSuccess").show();
-                
-                return this;
-            },
-                500: function(){
-                    $("#subscribeError").show("slow");
-                    return false;
-            }
-          }
-        });
+				var email = $("#emailAddressSubscribe").val();
 
 
+				$.ajax({
+					url: '/addToSubscribe',
+					type: 'POST',
+					data:{email:email,},
+					cache: false,
+					statusCode : {
+						200: function(){
+							$("#subscribeSuccess").show();
+							return this;
+						},
+						500: function(){
+							$("#subscribeError").show("slow");
+							return false;
+						}
+					}
+				});
+				return false;
+			}
 
-            return false;
-        }
+			return this;
 
-        return this;
-
-        },
-        isNumber:function(n) {
-            return !isNaN(parseFloat(n)) && isFinite(n);
-        }
-});
-    return HomePage; 
+		},
+		isNumber:function(n) {
+			return !isNaN(parseFloat(n)) && isFinite(n);
+		}
+	});
+	return HomePage;
 });
